7dad3245b64674b2da4a95c4fbc52a02d3a3a47e
Lukasz Zdziarski <lukas.zdziarski@gmail.com>
2020-10-08 20:50:07 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pg/eti/aui/SocialPosting/SocialPostingApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.eti.aui.SocialPosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pg/eti/aui/SocialPosting/SocialPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.eti.aui.SocialPosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
